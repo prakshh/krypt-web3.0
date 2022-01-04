246e75b2021dae3463f44df3d390dad8e7452983
“prakshh” <prakashdas18@gmail.com>
2022-01-05 00:06:56 +0530

    Tailwind update, 3md's and 2mf's in Welcome.jsx

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -23,7 +23,7 @@ const Welcome = () => {
 
     }
     const handleSubmit = () => {
-        
+
     }
 
     return (
@@ -66,7 +66,7 @@ const Welcome = () => {
                         </div>
                     </div>
                 </div>
-                <div className="flex flex-col flex-1 items-center justify-start w-full md:mt-0 mt-10">
+                <div className="flex flex-col flex-1 items-center justify-start w-full mf:mt-0 mt-10">
                     <div className="p-3 justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card .white-glassmorphism ">
                         <div className="flex justify-between flex-col w-full h-full">
                             <div className="flex justify-between items-start">
