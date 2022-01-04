3f72d7bb9ea8b551043272f2e6eb5f26c2dd9bdf
“prakshh” <prakashdas18@gmail.com>
2022-01-04 22:14:53 +0530

    card Form

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -6,6 +6,17 @@ import { Loader } from './';
 
 const CommonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
 
+const Input = ({ placeholder, name, type, value, handleChange }) => (
+    <input
+      placeholder={placeholder}
+      type={type}
+      step="0.0001"
+      value={value}
+      onChange={(e) => handleChange(e, name)}
+      className="my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism"
+    />
+  );
+
 const Welcome = () => {
 
     const connectWallet = () => {
@@ -70,6 +81,14 @@ const Welcome = () => {
                                 </p>
                             </div>
                         </div>
+                    </div>
+                    <div className="p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism">
+                        <Input placeholder="Address To" name="addressTo" type="text" handleChange={() => {handleChange}} /> 
+                        <Input placeholder="Amount (ETH)" name="amount" type="number" handleChange={() => {handleChange}} /> 
+                        <Input placeholder="Keyword (Gif)" name="keyword" type="text" handleChange={() => {handleChange}} /> 
+                        <Input placeholder="Enter Message" name="message" type="text" handleChange={() => {handleChange}} />  
+
+                        <div className="h-[1px] w-full bg-gray-400 my-2" />
                     </div>
                 </div>
             </div>
