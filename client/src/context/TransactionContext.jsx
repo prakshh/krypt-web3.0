f7e38993a104ed00d93dabdaa6c2ad4c55b35029
“prakshh” <prakashdas18@gmail.com>
2022-01-07 00:23:11 +0530

    form data fetching

--- client/src/context/TransactionContext.jsx
+++ client/src/context/TransactionContext.jsx
@@ -7,7 +7,7 @@ export const TransactionContext = React.createContext();
 
 const { ethereum } = window;
 
-const createEthereumContract = () => {
+const getEthereumContract = () => {
   const provider = new ethers.providers.Web3Provider(ethereum);
   const signer = provider.getSigner();
   const transactionsContract = new ethers.Contract(contractAddress, contractABI, signer);
@@ -20,7 +20,13 @@ console.log({
 }
 
 export const TransactionsProvider = ({ children }) => {
-  const [currentAccount, setCurrentAccount] = useState('initialState');
+  const [currentAccount, setCurrentAccount] = useState('');
+  const [formData, setFormData] = useState({ addressTo: '', amount: '', keyword: '', message: '' });
+
+  const handleChange = (e, name) => {
+    setFormData((prevState) => ({ ...prevState, [name]: e.target.value }));
+  };
+
 
 const checkIfWalletIsConnected = async () => {
   try{
@@ -40,10 +46,7 @@ const checkIfWalletIsConnected = async () => {
 
       throw new Error("No ethereum object");
   }
-
-    
-
-    console.log(accounts);
+    // console.log(accounts);
 }
 
 const connectWallet = async () => {
@@ -60,12 +63,27 @@ const connectWallet = async () => {
   }
 };
 
+const sendTransaction = async () => {
+  try{
+    if (!ethereum) return alert("Please install MetaMask.");
+
+    // get the data from the form
+    const { addressTo, amount, keyword, message } = formData;
+    getEthereumContract();
+  } 
+  catch (error) {
+    console.log(error);
+
+    throw new Error("No ethereum object");
+  }
+}
+
 useEffect(() => {
     checkIfWalletIsConnected();
   }, []);
  
 return (
-        <TransactionContext.Provider value={{ connectWallet, currentAccount }}>
+        <TransactionContext.Provider value={{ connectWallet, currentAccount, formData, setFormData, handleChange, sendTransaction }}>
           {children}
         </TransactionContext.Provider>
       );
