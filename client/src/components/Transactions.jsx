6a2368738ea449c18aad702bdee052a0ce3898f4
“prakshh” <prakashdas18@gmail.com>
2022-01-07 19:09:55 +0530

    simply returning a string in Latest Transactions

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -3,6 +3,12 @@ import React, { useContext } from "react";
 import { TransactionContext } from "../context/TransactionContext";
 
 import dummyData from "../utils/dummyData";
+import { shortenAddress } from "../utils/shortenAddress";
+
+const TransactionsCard = ({ addressTo, addressFrom, timestamp, message, keyword, amount, url }) => {
+    // const gifUrl = useFetch({ keyword });
+    return 'transaction';
+}
 
 const Transactions = () => {
     const { transactions, currentAccount } = useContext(TransactionContext);
@@ -21,6 +27,11 @@ const Transactions = () => {
                         Connect your account to see the latest transactions
                     </h3>
                     )}
+                    <div className="flex flex-wrap justify-center items-center mt-10">
+                        {dummyData.reverse().map((transaction, i) => (
+                            <TransactionsCard key={i} {...transaction} />
+                        ))}
+                    </div>
              </div>
         </div>
     );
