bc5e516b89651ef1741b1bb55754c0f4d080433a
“prakshh” <prakashdas18@gmail.com>
2022-01-07 12:46:02 +0530

    shortening account Address

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -4,6 +4,7 @@ import { SiEthereum } from 'react-icons/si';
 import { BsInfoCircle } from 'react-icons/bs';
 
 import { TransactionContext } from "../context/TransactionContext";
+import { shortenAddress } from "../utils/shortenAddress";
 import { Loader } from './';
 
 const CommonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
@@ -86,7 +87,8 @@ const Welcome = () => {
                             <div>
                                 <p className="text-white font-light text-sm">
                                     {/* Address */}
-                                    {currentAccount}
+                                    {/* {currentAccount} */}
+                                    {shortenAddress(currentAccount)}
                                 </p>
                                 <p className="text-white font-semibold text-lg mt-1">
                                     Ethereum
