de2d7aea1778d471684f11140e550d74ea9a85a2
“prakshh” <prakashdas18@gmail.com>
2022-01-07 20:27:20 +0530

    Address from every single transaction

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -6,8 +6,23 @@ import dummyData from "../utils/dummyData";
 import { shortenAddress } from "../utils/shortenAddress";
 
 const TransactionsCard = ({ addressTo, addressFrom, timestamp, message, keyword, amount, url }) => {
-    // const gifUrl = useFetch({ keyword });
-    return 'transaction';
+    return (
+        <div className="bg-[#181918] m-4 flex flex-1
+            2xl:min-w-[450px]
+            2xl:max-w-[500px]
+            sm:min-w-[270px]
+            sm:max-w-[300px]
+            flex-col p-3 rounded-md hover:shadow-2xl"
+        >
+            <div className="flex flex-col items-center w-full mt-3">
+                <div className="flex justify-start w-full mb-6 p-2">
+                    <a href={`https://ropsten.etherscan.io/address/${addressFrom}`} target="_blank" rel="noreferrer">
+                        <p className="text-white text-base">From: {shortenAddress(addressFrom)}</p>
+                    </a>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 const Transactions = () => {
