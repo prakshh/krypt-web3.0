96bf4f4f061fb39311bcf031ee34b4abe856e855
“prakshh” <prakashdas18@gmail.com>
2022-01-08 18:24:26 +0530

    implementing Loader

--- client/src/context/TransactionContext.jsx
+++ client/src/context/TransactionContext.jsx
@@ -7,7 +7,7 @@ export const TransactionContext = React.createContext();
 
 const { ethereum } = window;
 
-const getEthereumContract = () => {
+const createEthereumContract = () => {
   const provider = new ethers.providers.Web3Provider(ethereum);
   const signer = provider.getSigner();
   const transactionsContract = new ethers.Contract(contractAddress, contractABI, signer);
